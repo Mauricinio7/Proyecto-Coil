1292ae667f1752ac940783ba2e9cc79e26891ab0
Gabriel Armas <armasgabriel29@gmail.com>
2024-05-20 22:39:26 -0600

    Se hizo la conexion a la bd

---
+++ utilidades/Constantes.java
@@ -0,0 +1,14 @@
+package ProyectoCoil.utilidades;
+
+public class Constantes {
+    
+    public static final String DRIVER = "com.mysql.jdbc.Driver";
+    public static final String NOMBRE_BD = "bdcoil";
+    public static final String HOSTNAME = "127.0.1";
+    public static final String PUERTO = "3306";
+    public static final String USUARIO = "coiluser";
+    public static final String PASSWORD = "coil123";
+    public static final String MENSAJE_ERROR_CONEXION = "No disponible, inténtelo mas tarde";
+    public static final String KEY_ERROR = "error";
+    public static final String KEY_MENSAJE = "mensaje";
+}
