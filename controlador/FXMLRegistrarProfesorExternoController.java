6e71639ba8e4b06a570093fa06226edc6ca405f4
Gabriel Armas <armasgabriel29@gmail.com>
2024-05-23 16:45:46 -0600

    Se crearon los archivos de vista y controlador del CU04

---
+++ controlador/FXMLRegistrarProfesorExternoController.java
@@ -0,0 +1,14 @@
+package coilvic.controlador;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+public class FXMLRegistrarProfesorExternoController implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
