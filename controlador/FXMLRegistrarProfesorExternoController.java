e9a0d30d43e85b0dd3ce14fec03850ab4f51dc07
Gabriel Armas <armasgabriel29@gmail.com>
2024-05-23 17:49:59 -0600

    Se corrigio la vista y se prepararon los controlador del cu04

--- controlador/FXMLRegistrarProfesorExternoController.java
+++ controlador/FXMLRegistrarProfesorExternoController.java
@@ -2,15 +2,42 @@ package coilvic.controlador;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.animation.TranslateTransition;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
+import javafx.util.Duration;
 
 public class FXMLRegistrarProfesorExternoController implements Initializable {
 
     @FXML
     private Pane panelDeslisante;
+    @FXML
+    private TextField tfBusquedaProfesor;
+    @FXML
+    private Button btnCancelar;
+    @FXML
+    private TableView<?> tvProfesoresExternos;
+    @FXML
+    private TableColumn colNombre;
+    @FXML
+    private TableColumn colCorreo;
+    @FXML
+    private TableColumn colIdioma;
+    @FXML
+    private TableColumn colInstitucion;
+    @FXML
+    private TableColumn colPais;
+    @FXML
+    private TableColumn colTelefono;
+    @FXML
+    private TableColumn colColaboracion;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
@@ -19,10 +46,26 @@ public class FXMLRegistrarProfesorExternoController implements Initializable {
 
     @FXML
     private void salePanel(MouseEvent event) {
+        TranslateTransition transicion = new TranslateTransition();
+        transicion.setDuration(Duration.millis(500));
+        transicion.setNode(panelDeslisante);
+        transicion.setToX(0);
+        
+        transicion.play();
     }
 
     @FXML
     private void entraPanel(MouseEvent event) {
+        TranslateTransition transicion = new TranslateTransition();
+        transicion.setDuration(Duration.millis(500));
+        transicion.setNode(panelDeslisante);
+        transicion.setToX(230);
+        
+        transicion.play();
+    }
+
+    @FXML
+    private void btnAnadir(ActionEvent event) {
     }
     
 }
