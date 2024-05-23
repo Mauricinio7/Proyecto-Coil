433a2118223fb470947a3bfd8f7d08392dab50a5
Gabriel Armas <armasgabriel29@gmail.com>
2024-05-23 17:30:23 -0600

    Se cargo el fxml del cu04 en el main de coil

--- maincoilvic/CoilVic.java
+++ maincoilvic/CoilVic.java
@@ -14,8 +14,8 @@ public class CoilVic extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("../vista/FXMLLogin.fxml"));
-        //Parent root = FXMLLoader.load(getClass().getResource("vista/FXMLRegistrarProfesorExterno.fxml"));
+        //Parent root = FXMLLoader.load(getClass().getResource("../vista/FXMLLogin.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("../vista/FXMLRegistrarProfesorExterno.fxml"));
         
         Scene scene = new Scene(root);
         
