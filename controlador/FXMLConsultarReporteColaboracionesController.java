d4352c0897e629b81057baad53146a830e528278
Gabriel Armas <armasgabriel29@gmail.com>
2024-05-25 09:03:53 -0600

    Se creo la vista del CU06

---
+++ controlador/FXMLConsultarReporteColaboracionesController.java
@@ -0,0 +1,53 @@
+package coilvic.controlador;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.animation.TranslateTransition;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.util.Duration;
+
+public class FXMLConsultarReporteColaboracionesController implements Initializable {
+
+    @FXML
+    private Pane panelDeslisante;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void btnSalir(ActionEvent event) {
+    }
+
+    @FXML
+    private void cbPeriodo(ActionEvent event) {
+    }
+
+    @FXML
+    private void btnDescargar(ActionEvent event) {
+    }
+    
+    @FXML
+    private void salePanel(MouseEvent event) {
+        TranslateTransition transicion = new TranslateTransition();
+        transicion.setDuration(Duration.millis(500));
+        transicion.setNode(panelDeslisante);
+        transicion.setToX(0);
+        transicion.play();
+    }
+
+    @FXML
+    private void entraPanel(MouseEvent event) {
+        TranslateTransition transicion = new TranslateTransition();
+        transicion.setDuration(Duration.millis(500));
+        transicion.setNode(panelDeslisante);
+        transicion.setToX(230);
+        transicion.play();
+    }
+    
+}
