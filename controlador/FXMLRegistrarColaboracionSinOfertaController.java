47d3481ca6f95d306f5cd0f09e8e34a1984fd3ea
Gabriel Armas <armasgabriel29@gmail.com>
2024-06-12 23:29:44 -0600

    Se agrego el departamento en el pojo de oferta de colaboracion y de adapto el CU-18 para usarlo

--- controlador/FXMLRegistrarColaboracionSinOfertaController.java
+++ controlador/FXMLRegistrarColaboracionSinOfertaController.java
@@ -132,7 +132,6 @@ public class FXMLRegistrarColaboracionSinOfertaController implements Initializab
         cargarDatosProfesor();
         cargarDatosOfertaColaboracionSeleccionada();
         cargarEstadoComponentes();
-        cargarDepartamentos(cbAreaAcademica.getValue());
     }
 
     private void cargarEstadoComponentes() {
