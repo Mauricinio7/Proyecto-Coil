1a08e10e7ca3df89ca6bf287ba394a86d85189c1
Gabriel Armas <armasgabriel29@gmail.com>
2024-06-14 00:22:13 -0600

    Se borro el metodo para ajustar las fechas de la colaboracion

--- controlador/FXMLRegistrarColaboracionSinOfertaController.java
+++ controlador/FXMLRegistrarColaboracionSinOfertaController.java
@@ -113,7 +113,7 @@ public class FXMLRegistrarColaboracionSinOfertaController implements Initializab
         ofertaColaboracion.setPeriodo("Enero - Junio 2020");
         ofertaColaboracion.setTemaInteres("Tema de interés");
         ofertaColaboracion.setIdDepartamento(103);
-        inicializarValores(profesorUv, ofertaColaboracion);
+        inicializarValores(profesorUv);
         //Eliminar termina
     }    
     
@@ -306,11 +306,6 @@ public class FXMLRegistrarColaboracionSinOfertaController implements Initializab
                 }
             }
         });
-    }
-
-    private void configurarFechasInicioYFin() {
-        LocalDateTime fechaNTP = ConexionApacheNet.obtenerFechaHoraServidorNTP(Constantes.SERVIDOR_NTP);
-        //TODO
     }
 
     private boolean camposVacios(){
